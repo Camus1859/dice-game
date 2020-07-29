57ca72c152f3320083e003e6826dace1dfba31cd
Anderson Laventure <Laventurepsu1@gmail.com>
2020-07-29 10:51:21 -0400

    linked roll dice value to playerone current score

--- dice-game.js
+++ dice-game.js
@@ -1,29 +1,43 @@
+let dice = 0;
+
 let rollDice = document.getElementById('roll-dice')
 rollDice.addEventListener('click', randomDice)
-
 function randomDice(){
-  let dice = Math.floor(Math.random() * 6) + 1
+  dice = Math.floor(Math.random() * 6) + 1
   let diceDOM = document.querySelector('.dice')
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".gif";
-}
+  document.getElementById('player-one-current-score').textContent = dice
 
+}
 
 function displayOnStart(){
- let totalPoints = document.querySelectorAll('.total-points-number-both')
-let currentPoints = document.querySelectorAll('.points-current-number')
-let cashInButton = document.getElementById('cash-in-button')
+  let totalPoints = document.querySelectorAll('.total-points-number-both')
+  let currentPoints = document.querySelectorAll('.points-current-number')
+  let cashInButton = document.getElementById('cash-in-button')
+    totalPoints.forEach(item => {
+      item.style.visibility  = "hidden"
+    })
+    currentPoints.forEach(item => {
+      item.style.visibility = "hidden"
+    })
+    cashInButton.style.display =  "none"
+}
+displayOnStart();
 
-totalPoints.forEach(item => {
-  item.style.visibility  = "hidden"
-})
-currentPoints.forEach(item => {
-  item.style.visibility = "hidden"
-})
-
-cashInButton.style.display =  "none"
-
-
+let startButton = document.getElementById('start')
+startButton.addEventListener('click', scoresAppear)
+function scoresAppear(){
+  let totalPoints = document.querySelectorAll('.total-points-number-both')
+  let currentPoints = document.querySelectorAll('.points-current-number')
+  let cashInButton = document.getElementById('cash-in-button')
+    totalPoints.forEach(item => {
+      item.style.visibility  = "visible"
+    })
+    currentPoints.forEach(item => {
+      item.style.visibility = "visible"
+    })
+    cashInButton.style.display =  "block"
 }
 
-displayOnStart()
+
